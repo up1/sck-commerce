0d655ef9d2850c0f7301343b3424dfaca2c4af51
supagrid <supagrid@computerlogy.com>
2018-04-01 17:01:45 +0700

    fix

---
+++ ui/__mock__/products.js
@@ -0,0 +1,81 @@
+module.exports = [{
+  "id": "d87da073-c38e-4035-ac58-c10ce24c29e5",
+  "productId": "3a8fe534-5399-4e15-92cf-9f6d4b84b13e",
+  "price": 63,
+  "name": "enema",
+  "imgUrl": "https://robohash.org/remdoloresitaque.jpg?size=50x50&set=set1",
+  "brand": "Saline",
+  "rating": 2
+}, {
+  "id": "aa22ec53-3ae7-45f9-9c29-dc74a30b64bc",
+  "productId": "54a69a56-dafd-4d96-b3c5-ec1ccf652a7c",
+  "price": 50,
+  "name": "Salicylic Acid",
+  "imgUrl": "https://robohash.org/maximetotamnumquam.png?size=50x50&set=set1",
+  "brand": "Wart Remover Liquid",
+  "rating": 1
+}, {
+  "id": "36f7e749-a2dd-4be8-b53c-92a5f9e9f8ed",
+  "productId": "dd17edef-4826-44d4-9b1b-748317cacc38",
+  "price": 25,
+  "name": "Naproxen Sodium",
+  "imgUrl": "https://robohash.org/veleumrepellat.png?size=50x50&set=set1",
+  "brand": "Naproxen Sodium",
+  "rating": 4
+}, {
+  "id": "8fcfe6dc-a791-4087-b52d-7bbcd638aa2c",
+  "productId": "373c48fd-75cb-44f0-89cb-67447ab89f28",
+  "price": 20,
+  "name": "equaline childrens cold",
+  "imgUrl": "https://robohash.org/etfugiatdolores.jpg?size=50x50&set=set1",
+  "brand": "Acetaminophen, Chlorpheniramine Maleate, Dextromethorphan HBr, Phenylephrine HCl",
+  "rating": 3
+}, {
+  "id": "b1a169bd-c987-46bd-88b7-d778a0b15f28",
+  "productId": "5c546f6e-4311-4c78-8bed-fe5d857e159d",
+  "price": 3,
+  "name": "NATRUM PHOSPHORICUM",
+  "imgUrl": "https://robohash.org/etquivoluptas.bmp?size=50x50&set=set1",
+  "brand": "NATRUM PHOSPHORICUM",
+  "rating": 2
+}, {
+  "id": "548eaf80-16ea-43f4-b485-99a6ed32cf66",
+  "productId": "1db1aba8-4e42-4205-a746-19f736602330",
+  "price": 15,
+  "name": "Cricket",
+  "imgUrl": "https://robohash.org/quiexvel.bmp?size=50x50&set=set1",
+  "brand": "Cricket",
+  "rating": 3
+}, {
+  "id": "0a107ea9-d7a8-44b5-b3ba-4302f3e3b263",
+  "productId": "5ee00b97-de3f-4af2-818b-9ec2abdad253",
+  "price": 31,
+  "name": "OXYGEN",
+  "imgUrl": "https://robohash.org/ipsamcommodirem.png?size=50x50&set=set1",
+  "brand": "OXYGEN",
+  "rating": 5
+}, {
+  "id": "bdb01570-318a-4778-aaf9-9b08e2a38f56",
+  "productId": "7bd9c8de-e2a8-4d71-91f4-6b9442ccc291",
+  "price": 76,
+  "name": "Lipo-Chord",
+  "imgUrl": "https://robohash.org/corporisomnisrerum.jpg?size=50x50&set=set1",
+  "brand": "Homeopathic Liquid",
+  "rating": 3
+}, {
+  "id": "30ce9986-c3c2-4ae3-b1b2-2f8566935084",
+  "productId": "ce2c6b75-8602-4f05-986b-1ba1cb680ec0",
+  "price": 82,
+  "name": "JANUMET",
+  "imgUrl": "https://robohash.org/reiciendisquiaiusto.bmp?size=50x50&set=set1",
+  "brand": "SITAGLIPTIN and METFORMIN HYDROCHLORIDE",
+  "rating": 4
+}, {
+  "id": "0dcc5a95-7a38-4646-acbd-80cf1cf785ae",
+  "productId": "5df3c6fc-f8c3-4d2c-a01c-916c13a250bb",
+  "price": 59,
+  "name": "LBEL HYDRATESS",
+  "imgUrl": "https://robohash.org/quasporrovoluptates.png?size=50x50&set=set1",
+  "brand": "Octinoxate and Oxybenzone",
+  "rating": 5
+}]
