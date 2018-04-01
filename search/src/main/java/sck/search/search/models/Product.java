983081d706e5bb3064fac7f6c6fb3704181ff465
PhatSriwichai <phattharaphon.sriwichai@gmail.com>
2018-04-01 15:39:04 +0700

    add field Product

--- search/src/main/java/sck/search/search/models/Product.java
+++ search/src/main/java/sck/search/search/models/Product.java
@@ -1,5 +1,12 @@
 package sck.search.search.models;
 
 public class Product {
+    private String productId;
+    private int price;
+    private String name;
+    private String imgUrl;
+    private String brand;
+    private String rating;
+
     
 }
